d995f5fd100021c2f7f2a7c7953f02694d1468f2
Arnav Raj <arnavrajcodes@gmail.com>
2025-04-13 18:22:49 +0530

    safsafvdf

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    historyApiFallback: true,  // Ensures React Router handles routing on page reload
-  },
 })
